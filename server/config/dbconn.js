1303161f314b0a6f6685e4597a1de863854f7a59
Mohamed Saad <131729886+mohamedsaadDev@users.noreply.github.com>
2023-12-28 14:39:46 +0200

    Update dbconn.js

--- server/config/dbconn.js
+++ server/config/dbconn.js
@@ -1,11 +1,14 @@
 const mongoose =  require('mongoose')
 const connectDB = async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URL).then(()=>{
-            console.log("Connect to Mongoose successfully")
-        })
-    }catch(err){
-        console.log(err)
-    }
+    try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1);
+  }
 }
 module.exports = connectDB
